368a1bd69ac1b087fea964cd6915a38a55152a81
amogus-spectator <177993284+amogus-spectator@users.noreply.github.com>
2025-08-28 21:59:26 +0700

    Positive character groups
    The tests are ok now.

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <algorithm>
 #include <cctype>
+#include <limits>
+#include <cstddef>
+#include <vector>
 
 
 bool digit_check(const std::string& s) {
@@ -25,16 +28,59 @@ bool word_check(const std::string& s) {
     }
     return check_flag;
 }
+bool match_meta_positive_character_group(const std::string& s) {
+    size_t first_delim = s.find("[");
+    size_t second_delim = s.find("]");
+    if (first_delim == std::string::npos || second_delim == std::string::npos) {
+        return false;
+    }
+    int sniffer = static_cast<int>(second_delim - first_delim);
+    if (sniffer > 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+std::vector<char> split_string_into_char(const std::string& s) {
+    std::vector<char> split;
+    for (char c : s) {
+        split.push_back(c);
+    }
+    return split;
+}
+
+bool match_precise_positive_character_group(const std::string& s, const std::string& input_line) {
+        size_t first_delim = s.find("[");
+        size_t second_delim = s.find("]");
+        std::string matching_string = s.substr(first_delim+1).substr(0, second_delim-1);
+        std::cout << matching_string << std::endl;
+        std::vector<char> matching_char_vector = split_string_into_char(matching_string);
+        for (char c : matching_char_vector) {
+            std::cout << c << std::endl;
+            size_t matcher = input_line.find(c);
+            if (matcher == std::string::npos) {
+                return true;
+            }
+        }
+        return false;
+}
 
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
+    std::cout << "match_meta: " << static_cast<int>(match_meta_positive_character_group(pattern)) << std::endl;
     if (pattern.length() == 1) {
         return input_line.find(pattern) != std::string::npos;
     } else if (pattern == "\\d") {
         bool digit_flag = digit_check(input_line);
-        return digit_flag? true: false;
+        return digit_flag;
     } else if (pattern == "\\w") {
         bool word_flag = word_check(input_line);
-        return word_flag? true:false;
+        return word_flag;
+    } else if (match_meta_positive_character_group(pattern) == true) {
+        std::cout << "checking..." << std::endl;
+        bool positive_charater_group_flag = match_precise_positive_character_group(pattern, input_line);
+        std::cout << "precise: " << positive_charater_group_flag << std::endl;
+        return positive_charater_group_flag;
     }
     else {
         throw std::runtime_error("Unhandled pattern " + pattern);
@@ -69,10 +115,10 @@ int main(int argc, char* argv[]) {
     //
     try {
        if (match_pattern(input_line, pattern)) {
-            std::cout << "0";
+            std::cout << "Result: 0";
             return 0;
         } else {
-            std::cout << "1";
+            std::cout << "Result: 1";
             return 1;
         }
     } catch (const std::runtime_error& e) {

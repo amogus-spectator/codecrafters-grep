b7ae34ef2bd828c6640886c8ca54a9edd27b8d8f
amogus-spectator <177993284+amogus-spectator@users.noreply.github.com>
2025-08-29 20:35:37 +0700

    Update Server.cpp

--- src/Server.cpp
+++ src/Server.cpp
@@ -57,7 +57,6 @@ bool match_precise_positive_character_group(const std::string& s, const std::str
         std::cout << matching_string << std::endl;
         std::vector<char> matching_char_vector = split_string_into_char(matching_string);
         for (char c : matching_char_vector) {
-            std::cout << c << std::endl;
             size_t matcher = input_line.find(c);
             if (matcher == std::string::npos) {
                 return true;
